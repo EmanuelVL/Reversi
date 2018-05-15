22cdce8e18f34c7de8d0ad14c9f47718b8a169c2
EmanuelVL <33427332+EmanuelVL@users.noreply.github.com>
2018-05-15 10:43:35 -0700

    Update 180515.cpp

--- 180515.cpp
+++ 180515.cpp
@@ -1,13 +1,9 @@
-
 #include <iostream>
-
 #include <windows.h>
-
-using namespace std;
 
 #define LIGHTRED 12
 
-
+using namespace std;
 
 // Smaller board for testing purposes.  Actual size is 8x8.
 const int ROWS = 8;
@@ -19,9 +15,6 @@ const char HUMAN_PLAYER = 'X';
 
 const char COMPUTER_PLAYER = 'O';
 
-
-
-
 /****************************************************************************/
 
 // Display the board to the screen.
@@ -29,39 +22,20 @@ const char COMPUTER_PLAYER = 'O';
 // [in] board - Game board
 
 void DisplayBoard(const char board[][COLS])
-
 {
-
     cout << " ";
-
     for (int c = 0; c < COLS; c++)
-
         cout << c;
-
     cout << endl;
-
-
-    for (int r = 0; r < ROWS; r++)
-
-    {
-
+    for (int r = 0; r < ROWS; r++){
         cout << r;
-
         for (int c = 0; c < COLS; c++)
-
             cout << board[r][c];
-
         cout << endl;
-
     }
-
 }
 
-
-
 /****************************************************************************/
-
-
 
 // Place the disc on the board, flip the discs affected, and
 
@@ -78,276 +52,147 @@ void DisplayBoard(const char board[][COLS])
 // [in]  disc  - Player's disc to be placed (X or O)
 
 int PlaceDisc(char board[][COLS], int row, int col, char disc)
-
 {
-
     int num_flipped = 0;
-
-
+    
     if (board[row][col] != ' ')
-
         return 0;
-
-
 
     board[row][col] = disc;
 
-
-
     char opposing_disc;
-
+    
     if (disc == HUMAN_PLAYER)
-
         opposing_disc = COMPUTER_PLAYER;
-
     else
-
         opposing_disc = HUMAN_PLAYER;
-
-
 
     // Do we have discs to the right to flip?
 
     int disc_pos = -1;
-
-    for (int c = col + 1; c < COLS && board[row][c] != ' ' && disc_pos == -1; c++)
-
-    {
-
+    
+    for (int c = col + 1; c < COLS && board[row][c] != ' ' && disc_pos == -1; c++){
         if (board[row][c] == disc)
-
             disc_pos = c;
-
     }
-
-
 
     // Make sure we found a disc and that it is at least 2 spots away
 
-    if (disc_pos != -1 && disc_pos > col + 1)
-
-    {
-
+    if (disc_pos != -1 && disc_pos > col + 1){
         // Flip discs to the right
-
-        for (int c = col + 1; c < disc_pos; c++)
-
-        {
-
+        for (int c = col + 1; c < disc_pos; c++){
             board[row][c] = disc;
-
             num_flipped++;
-
         }
-
     }
-
-
 
     // Do we have discs to the left to flip?
 
     disc_pos = -1;
-
-    for (int c = col - 1; c >= 0 && board[row][c] != ' ' && disc_pos == -1; c--)
-
-    {
-
+    
+    for (int c = col - 1; c >= 0 && board[row][c] != ' ' && disc_pos == -1; c--){
         if (board[row][c] == disc)
-
             disc_pos = c;
-
     }
-
-
 
     //if (disc_pos > -1) cout << "left disc_pos = " << disc_pos << endl;
 
-
-
     // Make sure we found a disc and that it is at least 2 spots away
 
-    if (disc_pos != -1 && disc_pos < col - 1)
-
-    {
-
+    if (disc_pos != -1 && disc_pos < col - 1){
         // Flip discs to the left
-
-        for (int c = col - 1; c > disc_pos; c--)
-
-        {
-
+        for (int c = col - 1; c > disc_pos; c--){
             board[row][c] = disc;
-
             num_flipped++;
-
         }
-
     }
-
-
 
     // Do we have discs above to flip?
 
     disc_pos = -1;
 
-    for (int r = row - 1; r >= 0 && board[r][col] != ' ' && disc_pos == -1; r--)
-
-    {
-
+    for (int r = row - 1; r >= 0 && board[r][col] != ' ' && disc_pos == -1; r--){
         if (board[r][col] == disc)
-
             disc_pos = r;
-
     }
-
-
 
     // Make sure we found a disc and that it is at least 2 spots away
 
-    if (disc_pos != -1 && disc_pos < row - 1)
-
-    {
-
+    if (disc_pos != -1 && disc_pos < row - 1){
         // Flip discs above
-
-        for (int r = row - 1; r > disc_pos; r--)
-
-        {
-
+        for (int r = row - 1; r > disc_pos; r--){
             board[r][col] = disc;
-
             num_flipped++;
-
         }
-
     }
-
-
 
     // Do we have discs below to flip?
 
     disc_pos = -1;
 
-    for (int r = row + 1; r < ROWS && board[r][col] != ' ' && disc_pos == -1; r++)
-
-    {
-
+    for (int r = row + 1; r < ROWS && board[r][col] != ' ' && disc_pos == -1; r++){
         if (board[r][col] == disc)
-
             disc_pos = r;
-
     }
-
-
 
     // Make sure we found a disc and that it is at least 2 spots away
-
-    if (disc_pos != -1 && disc_pos > row + 1)
-
-    {
-
+    
+    if (disc_pos != -1 && disc_pos > row + 1){
         // Flip discs below
-
-        for (int r = row + 1; r < disc_pos; r++)
-
-        {
-
+        for (int r = row + 1; r < disc_pos; r++){
             board[r][col] = disc;
-
             num_flipped++;
-
         }
-
     }
-
 
     // Do we have discs diagnally up-left to flip?
 
     disc_pos = -1;
 
     int c = col - 1;
-
-    for (int r = row - 1; c >= 0 && r >= 0 && board[r][c] != ' ' && disc_pos == -1; r--)
-
-    {
-
+    
+    for (int r = row - 1; c >= 0 && r >= 0 && board[r][c] != ' ' && disc_pos == -1; r--){
         if (board[r][c] == disc)
-
             disc_pos = r;
-
         c--;
-
     }
-
-
 
     // Make sure we found a disc and that it is at least 2 spots away
 
-    if (disc_pos != -1 && disc_pos < row - 1)
-
-    {
+    if (disc_pos != -1 && disc_pos < row - 1){
 
         c = col - 1;
 
-        for (int r = row - 1; r > disc_pos; r--)
-
-        {
-
+        for (int r = row - 1; r > disc_pos; r--){
             board[r][c] = disc;
-
             num_flipped++;
-
             c--;
-
         }
-
     }
 
-
-
-    // Do we have discs diagnally up-right to flip?
+    // Do we have discs diagonally up-right to flip?
 
     disc_pos = -1;
 
     c = col + 1;
 
-    for (int r = row - 1; c < COLS && r >= 0 && board[r][c] != ' ' && disc_pos == -1; r--)
-
-    {
-
+    for (int r = row - 1; c < COLS && r >= 0 && board[r][c] != ' ' && disc_pos == -1; r--){
         if (board[r][c] == disc)
-
             disc_pos = r;
-
         c++;
-
     }
-
-
 
     // Make sure we found a disc and that it is at least 2 spots away
 
-    if (disc_pos != -1 && disc_pos < row - 1)
-
-    {
-
+    if (disc_pos != -1 && disc_pos < row - 1){
         c = col + 1;
-
-        for (int r = row - 1; r > disc_pos; r--)
-
-        {
-
+        
+        for (int r = row - 1; r > disc_pos; r--){
             board[r][c] = disc;
-
             num_flipped++;
-
             c++;
-
         }
-
     }
-
-
 
     // Do we have discs diagnally down-left to flip?
 
@@ -355,19 +200,12 @@ int PlaceDisc(char board[][COLS], int row, int col, char disc)
 
     c = col - 1;
 
-    for (int r = row + 1; c >= 0 && r < ROWS && board[r][c] != ' ' && disc_pos == -1; r++)
-
-    {
-
+    for (int r = row + 1; c >= 0 && r < ROWS && board[r][c] != ' ' && disc_pos == -1; r++){
         if (board[r][c] == disc)
-
             disc_pos = r;
-
         c--;
 
     }
-
-
 
     // Make sure we found a disc and that it is at least 2 spots away
 
